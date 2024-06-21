877de4942ba8b44771824203509a772b4c2f3802
Gilberto <gilberto.melo.paiva@gmail.com>
2024-06-20 23:17:56 -0300

    Ajuste de AgendamentoService

--- src/main/java/br/com/gpaiva/susacess/service/AgendamentoService.java
+++ src/main/java/br/com/gpaiva/susacess/service/AgendamentoService.java
@@ -1,5 +1,9 @@
 package br.com.gpaiva.susacess.service;
 
+import br.com.gpaiva.susacess.model.Medico;
+import br.com.gpaiva.susacess.model.Paciente;
+import br.com.gpaiva.susacess.repository.MedicoRepository;
+import br.com.gpaiva.susacess.repository.PacienteRepository;
 import org.modelmapper.ModelMapper;
 import br.com.gpaiva.susacess.model.Agendamento;
 import br.com.gpaiva.susacess.model.dto.AgendamentoInputDTO;
@@ -12,13 +16,24 @@ import org.springframework.stereotype.Service;
 public class AgendamentoService {
     @Autowired
     private AgendamentoRepository agendamentoRepository;
-
     @Autowired
-    private ModelMapper modelMapper;
+    private MedicoRepository medicoRepository;
+    @Autowired
+    private PacienteRepository pacienteRepository;
 
     public AgendamentoOutDTO agendar(AgendamentoInputDTO agendamentoInputDTO) {
-        Agendamento agendamento = modelMapper.map(agendamentoInputDTO, Agendamento.class);
+        Agendamento agendamento = new Agendamento();
+        Medico medico = medicoRepository.findById(agendamentoInputDTO.getMedicoId()).orElseThrow();
+        Paciente paciente = pacienteRepository.findById(agendamentoInputDTO.getPacienteId()).orElseThrow();
+        agendamento.setMedico(medico);
+        agendamento.setPaciente(paciente);
+        agendamento.setDataHora(agendamentoInputDTO.getDataHora());
         agendamento = agendamentoRepository.save(agendamento);
-        return modelMapper.map(agendamento, AgendamentoOutDTO.class);
+        AgendamentoOutDTO agendamentoOutDTO = new AgendamentoOutDTO();
+        agendamentoOutDTO.setId(agendamento.getId());
+        agendamentoOutDTO.setMedicoId(medico.getId());
+        agendamentoOutDTO.setPacienteId(paciente.getId());
+        agendamentoOutDTO.setDataHora(agendamento.getDataHora());
+        return agendamentoOutDTO;
     }
 }
